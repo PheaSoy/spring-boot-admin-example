d4044c4728497f192e0c2a434d662b5b30958c01
Soy Phea <phea.soy@hatthabank.com>
2022-06-12 11:54:13 +0700

    Added codes

---
+++ src/main/java/org/soyphea/springbootadminapp/SpringBootAdminAppApplication.java
@@ -0,0 +1,15 @@
+package org.soyphea.springbootadminapp;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdminAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/soyphea/springbootadminapp/SpringBootAdminAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soyphea.springbootadminapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
